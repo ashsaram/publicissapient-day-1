92b3a088e657c766e279bbe1aaa7988b67074a19
Ashritha Saram <ashsaram@publicisgroupe.net>
2021-04-27 19:05:05 +0530

    day1

---
+++ multi.java
@@ -0,0 +1,8 @@
+public class multi {
+  public static void main(String[] args) {
+   int num1 =5;
+   int num2 =2;
+   product = num1*num2;
+  System.out.println("The product of two given number is : "+product);
+     }
+  }
