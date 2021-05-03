d470b00af0dbcc3ae198b55a51e562c34158a716
Ashritha Saram <ashsaram@publicisgroupe.net>
2021-05-03 18:51:48 +0530

    first commit

---
+++ day1/src/test/java/day1/AddTwoNumTest.java
@@ -0,0 +1,15 @@
+package day1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddTwoNumTest {
+
+	@Test
+	void test() {
+		
+		assertEquals(5,AddTwoNum.add(3,2));
+	}
+
+}
